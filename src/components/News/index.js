7dd341c7a92a31d089d076836b0c68adf67526d4
ashokkmwt <ashok1481997@gmail.com>
2022-10-18 14:44:56 +0530

    UI updated and loading feature added

--- src/components/News/index.js
+++ src/components/News/index.js
@@ -1,12 +1,17 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import { useWindowSize } from 'react-use';
 import { API_ID } from '../../utils/helper'
 import Pagination from '../Pagination';
 import styles from './index.module.css'
 
 export default function News() {
 
+    const { width } = useWindowSize();
+
     const [users, setUsers] = useState([]);
+
+    const [loading, setLoading] = useState(false);
 
     const [totalUsers, setTotalUsers] = useState("");
 
@@ -17,6 +22,7 @@ export default function News() {
     const totalPages = Math.ceil(totalUsers / userPerPage);
 
     useEffect(() => {
+        setLoading(true);
         axios({
             method: 'get',
             url: `https://dummyapi.io/data/v1/user?page=${currentPage}&limit=10`,
@@ -24,6 +30,7 @@ export default function News() {
                 'app-id': API_ID
             }
         }).then(function (response) {
+            setLoading(false);
             setUsers(response.data.data);
             setTotalUsers(response.data.total);
             setUserPerPage(response.data.limit);
@@ -35,19 +42,17 @@ export default function News() {
 
     const prevPage = () => {
         if (currentPage > 1) {
-            setTimeout(function () {
-                setCurrentPage(currentPage - 1)
-            }, 1000)
+            setCurrentPage(currentPage - 1)
         }
     }
 
     const nextPage = () => {
         if (currentPage < totalPages) {
-            setTimeout(function () {
-                setCurrentPage(currentPage + 1)
-            }, 1000)
+            setCurrentPage(currentPage + 1)
         }
     }
+
+    const props = { currentPage, totalPages, prevPage, nextPage }
 
     return (
         <div className={styles.container}>
@@ -55,25 +60,31 @@ export default function News() {
                 <h1>Dummy Data</h1>
             </header>
 
-            <Pagination currentPage={currentPage} totalPages={totalPages} prevPage={prevPage} nextPage={nextPage} />
+            <Pagination {...props} />
+
+            {loading && <h1>Loading...</h1>}
 
             <main>
                 {users.map((user, index) => {
                     return (
-                        <div key={index} className={styles.section}>
+                        <div
+                            style={width > 800 ?
+                                index % 2 === 0 ? { paddingLeft: "10%" } : { paddingRight: "10%" } :
+                                index % 2 === 0 ? { paddingLeft: "25%" } : { paddingRight: "25%" }
+                            }
+                            key={index}
+                            className={styles.section} >
                             <div className={styles.newsBox}>
-                                <h4>{user.title}</h4>
                                 <div className={styles.image}>
                                     <img width="100%" height="100%" src={user.picture} alt={user.title} />
                                 </div>
-                                <p>Description</p>
-                                <h5>{user.firstName} {user.lastName}</h5>
+                                <h5>UserName: <span>{user.title}</span> {user.firstName} {user.lastName}</h5>
                             </div>
                         </div>
                     )
                 })}
 
-            </main>
-        </div>
+            </main >
+        </div >
     )
 }
